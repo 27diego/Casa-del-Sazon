68b735e730484bd703313515debca31813c2c2e7
Diego Vega <diego.vops@gmail.com>
2021-02-04 14:37:19 -0800

    made an edit to banner

--- LaCasaDelSazon/Views/SubViews/NotificationBanner.swift
+++ LaCasaDelSazon/Views/SubViews/NotificationBanner.swift
@@ -15,14 +15,13 @@ struct NotificationBanner: View {
             .foregroundColor(Color(#colorLiteral(red: 0.09019607843, green: 0.1137254902, blue: 0.1333333333, alpha: 1)))
             .overlay(
                 HStack {
-                    Image(systemName: "exclamationmark.circle")
+                    Image(systemName: "exclamationmark.triangle")
                         .resizable()
                         .aspectRatio(contentMode: .fill)
                         .frame(width: 23, height:23)
                         .foregroundColor(.white)
                     Text(text)
                         .foregroundColor(.white)
-//                        .padding()
                 }
                 .padding()
             )
